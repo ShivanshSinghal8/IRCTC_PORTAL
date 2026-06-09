ec624673386719f666af642c6c3d6b792a218fcd
ShivanshSinghal8 <shivanshwork8888@gmail.com>
2026-06-09 15:15:02 +0530

    File object added with path

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,11 +2,14 @@ package ticket.booking.services;
 
 import ticket.booking.entities.User;
 
+import java.io.File;
+
 public class UserBookingService {
     private User user;
     private static final String USER_PATH = "../LocalDB/User.json";
     public UserBookingService(User user) {
         this.user = user;
+        File users = new File(USER_PATH);
     }
 
 }
